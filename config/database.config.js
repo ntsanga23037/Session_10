2315b0820ad0b89c605d556a5a36c9fa5e8b81d1
ntsanga23037 <ntsanga23037@cusc.ctu.edu.vn>
2024-11-18 21:51:10 +0800

    first commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://ntsanga23037:trungsanga23037@cluster0.g3t8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
